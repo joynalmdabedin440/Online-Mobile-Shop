843a6ba0b2e5fea55f99741b314417b7ff9c8efd
joynalmdabedin <joynalmdabedin440@gmail.com>
2024-10-17 23:24:56 +0600

    add Mobile dokan website

---
+++ script.js
@@ -0,0 +1,11 @@
+const loadData = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+
+    const data = await  res.json()
+
+
+    console.log(data.data[0].image);
+    
+}
+
+loadData()
